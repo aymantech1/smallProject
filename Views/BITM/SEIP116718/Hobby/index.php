38e8e5750914ed5f2627d7ac822f1d15404465b8
Rakib Hossain <rakibhossaincse@gmail.com>
2016-04-06 14:20:08 +0600

    Check box CRUD Delete and show delete item completed

--- Views/BITM/SEIP116718/Hobby/index.php
+++ Views/BITM/SEIP116718/Hobby/index.php
@@ -3,8 +3,7 @@
     use App\BITM\SEIP116718\Hobby\Hobby;
     $allHobby = new Hobby();
     $allHobbyInfo = $allHobby->index();
-   
-?>
+
 ?>
 <!doctype html>
 <html class="no-js" lang="">
@@ -157,40 +156,3 @@
         </script>
     </body>
 </html>
-<!--
-<html>
-    <head>
-        <title>Hobby</title>
-    </head>
-    <body>
-        <a href="index.php">Home Page</a>
-        <a href="create.php">Create Book Title</a>
-        <a href="deleteitem.php">Delete item list</a>
-        
-        <table border="1">
-            <tr>
-                <th>Serial</th>
-                <th>Title</th>
-                <th>Action</th>
-            </tr>
-            <?php
-                $serial = 0;
-                foreach ($allHobbyInfo as $value) {
-                    $serial++;
-            ?>      
-                    <tr>
-                        <td><?php echo $serial?></td>
-                        <td><?php echo $value['title']?></td>
-                        <td>
-                            <a href="show.php?id=<?php echo $value['id']?>">Show Details</a>
-                            <a href="edit.php?id=<?php echo $value['id']?>">Edit</a>
-                            <a href="softDelete.php?id=<?php echo $value['id']?>">Delete</a>
-                        </td>
-                    </tr>
-            <?php
-                }
-            ?>
-        </table>
-    </body>
-</html>
--->
